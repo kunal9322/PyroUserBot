92c6332e7c09143d4832b21a29313cb992ca120a
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-04-06 09:35:26 +0530

    Add files via upload

--- PyroUserBot/helper/__init__.py
+++ PyroUserBot/helper/__init__.py
@@ -1,11 +1,14 @@
 import os
 import sys
 from pyrogram import Client
+
+
+
 def restart():
     os.execvp(sys.executable, [sys.executable, "-m", "PyroUserBot"])
+
 async def join(client):
     try:
-        await client.join_chat("PyroUserBot")
+        await client.join_chat("TheUpdatesChannel")
     except BaseException:
         pass
-
